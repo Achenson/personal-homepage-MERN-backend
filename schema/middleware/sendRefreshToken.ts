793af9a271b9d4add76ee24753064daa7a85497c
Achenson <ach.01.mail@gmail.com>
2022-12-23 18:51:23 +0100

    sendRefreshToken sameSite secure domain change2

--- schema/middleware/sendRefreshToken.ts
+++ schema/middleware/sendRefreshToken.ts
@@ -3,10 +3,10 @@ import {Response} from "express"
 module.exports = (res: Response, token: String) => {
   console.log("SENDING REFRESH TOKEN");
     res.cookie("jid", token, {
-      domain: "https://smoothtabs.onrender.com",
+      domain: "smoothtabs.onrender.com",
       httpOnly: true,
       // to prevent sending cookie in every request
-      path: "/refresh_token",
+      // path: "/refresh_token",
       // path: "/refresh_token",
       // if expires is not set, the cookie will be session only
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
